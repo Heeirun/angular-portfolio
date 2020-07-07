4ce81db36a3deb6b2fe2ebbe7ab98d38fc18d5cb
Heeirun <heeirun@MacBook-Pro.local>
2020-07-07 13:28:19 -0500

    seo ooptimization changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolio-client';
+export class AppComponent implements OnInit {
+  title = 'Heeirun Jayakumar';
+
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+    this.metaService.addTags([
+      {name: 'keywords', content: 'Heeirun Jayakumar'},
+      {name: 'description', content: 'Heeirun Jayakumar\'s personal website'},
+      {name: 'robots', content: 'index, follow'}
+    ]);
+  }
+
+  constructor(private titleService: Title, private metaService: Meta) {
+
+  }
 }
